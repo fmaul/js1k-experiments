31e84d938165fa432fd8eafb164b25033e9ea15e
Florian <f.maul@fme.de>
2018-08-10 16:18:51 +0200

    Some optimizations

--- 03-3d-cube/index.js
+++ 03-3d-cube/index.js
@@ -3,7 +3,7 @@ w = a.width;
 
 vm = (a) => a.map(e => [e]);
 mv = (m) => {
-    let v = [m[0][0], m[1][0]];
+    v = [m[0][0], m[1][0]];
     if (m.length > 2) v.push(m[2][0]);
     return v;
 }
@@ -14,6 +14,7 @@ tp = a => a[0].map((x, i) => a.map(y => y[i]));
 //dt = (a,b) => (a[0]-b[0])**2+(a[1]-b[1])**2+(a[2]-b[2])**2;
 dt = (a, b) => a.reduce((t, e, i) => t + (e - b[i]) ** 2, 0);
 
+/*
 P = [
     [-1, -1, -1],
     [1, -1, -1],
@@ -24,61 +25,43 @@ P = [
     [1, 1, 1],
     [-1, 1, 1]
 ];
+*/
+P = [];for(i=8; i--;)P.push([i%2,i%4<2?0:1,i%8<4?0:1].map(x=>x*2-1))
 
-let angle = 0;
+
+var angle = 0;
 c.translate(w / 2, h / 2);
 c.fillStyle = "rgba(0,0,0,0.1)";
+
 
 with(Math)(L = () => {
     angle += 0.03;
     c.strokeStyle = `rgb(${sin(angle*2)*128+128},${cos(angle)*128+128},${sin(angle)*128+128})`;
-    /*
-    let rotZ = [
-        [cos(angle), -sin(angle), 0],
-        [sin(angle), cos(angle), 0],
-        [0, 0, 1]
-    ];
-    let rotX = [
-        [1, 0, 0],
-        [0, cos(angle), -sin(angle)],
-        [0, sin(angle), cos(angle)],
-    ];
-    let rotY = [
-        [cos(angle), 0, -sin(angle)],
-        [0, 1, 0],
-        [sin(angle), 0, cos(angle)],
-    ];
-    */
-    let project = (p) => {
-        let rotated = mmv([
+    var project = (p) => {
+        var rotated = mmv([ // rot Y
             [cos(angle), 0, -sin(angle)],
-            [0, 1, 0],
+            [0, 1],
             [sin(angle), 0, cos(angle)],
-        ], p);
-        let z = 1 / (6 + sin(angle / 2) * 2 - rotated[2]);
-        let proj = [
-            [z, 0, 0],
-            [0, z, 0]
-        ];
+        ], mmv([ // rot Z
+            [cos(angle/4), -sin(angle/4)],
+            [sin(angle/4), cos(angle/4)],
+            [0, 0, 1]
+        ], p));
+        var z = 1 / (6 + sin(angle / 2) * 2 - rotated[2]);
 
-        return mmv(proj, rotated).map(v => v * w);
+        return mmv([
+            [z],
+            [0, z]
+        ], rotated).map(v => v * w);
     }
-    let C = (i, j) => {
+    var C = (i, j) => {
         c.beginPath();
-        let a = project(P[i]);
-        let b = project(P[j]);
+        var a = project(P[i]);
+        var b = project(P[j]);
         c.moveTo(a[0], a[1]);
         c.lineTo(b[0], b[1]);
         c.stroke();
-        /*
-        c.beginPath();
-        c.moveTo(a[0], a[1]);
-        c.arc(a[0], a[1], 5, 0, 2 * PI);
-        c.closePath();
-        c.fill();
-        */
-    }
-
+   }
     c.fillRect(-w / 2, -h / 2, w, h);
 
     /*
