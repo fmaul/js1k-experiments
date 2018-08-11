de4e340322d356e367a667a7b7521f3babce61c1
Florian <f.maul@fme.de>
2018-08-11 10:31:39 +0200

    paint cube faces

--- 03-3d-cube/index.js
+++ 03-3d-cube/index.js
@@ -1,18 +1,24 @@
 h = a.height;
 w = a.width;
 
+/*
 vm = (a) => a.map(e => [e]);
 mv = (m) => {
     v = [m[0][0], m[1][0]];
     if (m.length > 2) v.push(m[2][0]);
     return v;
 }
-mmv = (a, b) => mv(mm(a, vm(b)));
-mm = (a, b) => a.map(x => tp(b).map(y => dp(x, y)));
+*/
+/*
+M = (a, b) => a.map(x => tp(b).map(y => dp(x, y)));
 dp = (a, b) => a.map((x, i) => a[i] * b[i]).reduce((a, b) => a + b);
 tp = a => a[0].map((x, i) => a.map(y => y[i]));
+*/
+M = (a, b) => a.map(x => b[0].map((x, i) => b.map(y => y[i])).map(y => x.map((v, i) => x[i] * y[i]).reduce((a, b) => a + b)));
+
 //dt = (a,b) => (a[0]-b[0])**2+(a[1]-b[1])**2+(a[2]-b[2])**2;
-dt = (a, b) => a.reduce((t, e, i) => t + (e - b[i]) ** 2, 0);
+dt = (a,b) => a.reduce((t,e,i)=>t+(e-b[i])**2,0);
+
 
 /*
 P = [
@@ -31,39 +37,56 @@ P = [];for(i=8; i--;)P.push([i%2,i%4<2?0:1,i%8<4?0:1].map(x=>x*2-1))
 
 var angle = 0;
 c.translate(w / 2, h / 2);
-c.fillStyle = "rgba(0,0,0,0.1)";
 
 
 with(Math)(L = () => {
     angle += 0.03;
-    //c.strokeStyle = `rgb(${sin(angle*2)*128+128},${cos(angle)*128+128},${sin(angle)*128+128})`;
-    c.strokeStyle = "#fff";
+
     var project = (p) => {
-        var rotated = mmv([ // rot Y
+        var rotated = M([ // rot Y
             [cos(angle), 0, -sin(angle)],
             [0, 1],
             [sin(angle), 0, cos(angle)],
-        ], mmv([ // rot Z
+        ], M([ // rot Z
             [cos(angle/4), -sin(angle/4)],
             [sin(angle/4), cos(angle/4)],
             [0, 0, 1]
-        ], p));
+        ], p.map(e=>[e])));
         var z = 1 / (6 + sin(angle / 2) * 2 - rotated[2]);
 
-        return mmv([
+        return M([
             [z],
             [0, z]
-        ], rotated).map(v => v * w);
+        ], rotated).map(v => v[0] * w);
     }
-    var C = (i, j) => {
+    /*var C = (i, j) => {
         c.beginPath();
         var a = project(P[i]);
         var b = project(P[j]);
         c.moveTo(a[0], a[1]);
         c.lineTo(b[0], b[1]);
         c.stroke();
-   }
+    }*/
+
+   var D = (p) => {
+    c.beginPath();
+    p.map((o,i) => (o=project(o),i?c.lineTo(o[0], o[1]):c.moveTo(o[0], o[1]))
+    );
+    c.closePath();
+    c.fill();
+    c.stroke();
+
+    }
+    //c.globalCompositeOperation = 'normal';
+
+    c.lineWidth=3;
+    c.strokeStyle=c.fillStyle = "rgba(0,0,0,0.1)";
     c.fillRect(-w / 2, -h / 2, w, h);
+
+//c.globalCompositeOperation = 'lighter';
+   c.fillStyle = `rgb(${sin(angle*2)*128+128},${cos(angle)*128+128},${sin(angle)*128+128},0.5)`;
+
+  //  c.fillStyle = "rgba(255,0,0,0.1)";
 
     /*
     c.beginPath();
@@ -75,11 +98,13 @@ with(Math)(L = () => {
     });
     c.fill();
     */
-
+/*
     for (i = 8; i--;)
         for (j = i; j--;)
             if (dt(P[i], P[j]) == 4) C(i, j);
     //for (i = 4; i--;) C(i, (i + 1) % 4), C(i + 4, 4 + (i + 1) % 4), C(i, i + 4);
+*/
+    for(i=6;i--;)D(P.sort(a=>a[i/2]).slice(i%2*4,4+i%2*4));
 
-    requestAnimationFrame(L);
+     requestAnimationFrame(L);
 })();
--- 04-4d-hypercube/index.js
+++ 04-4d-hypercube/index.js
@@ -1,19 +1,20 @@
 h = a.height;
 w = a.width;
 
-vm = a => a.map(e => [e]);
-mv = a => a.map(e => e[0]);
-mmv = (a, b) => mv(mm(a, vm(b)));
-mm = (a, b) => a.map(x => tp(b).map(y => dp(x, y)));
+/*
+M = (a, b) => a.map(x => tp(b).map(y => dp(x, y)));
 dp = (a, b) => a.map((x, i) => a[i] * b[i]).reduce((a, b) => a + b);
 tp = a => a[0].map((x, i) => a.map(y => y[i]));
+*/
+M = (a, b) => a.map(x => b[0].map((x, i) => b.map(y => y[i])).map(y => x.map((v, i) => x[i] * y[i]).reduce((a, b) => a + b)));
+
 //dt = (a,b) => (a[0]-b[0])**2+(a[1]-b[1])**2+(a[2]-b[2])**2;
 dt = (a,b) => a.reduce((t,e,i)=>t+(e-b[i])**2,0);
 
 //P = [];
 //for (x=2;x--;) for (y=2;y--;) for (z=2;z--;) for (w=2;w--;) P.push([x,y,z,w].map(x=>x*2-1))
 //P.push([x*2-1,y*2-1,z*2-1,w*2-1]);
-P = [];for(i=16; i--;)P.push([i%2,i%4<2?0:1,i%8<4?0:1,i<8?0:1].map(x=>x*2-1))
+P = [];for(i=16; i--;)P.push([i%2,i%4<2?0:1,i%8<4?0:1,i<8?0:1].map(x=>x-.5))
 
 var angle=1
 var angle2 = 1;
@@ -23,39 +24,40 @@ with(Math)(L = () => {
 
     angle-=0.001;
     angle2+=0.01;
-   // c.strokeStyle = `rgb(${sin(angle2*2)*128+128},${cos(angle2)*128+128},${sin(angle2)*128+128})`;
-    c.strokeStyle = "#fff";
 
+    //c.strokeStyle = `rgb(${sin(angle2*2)*128+128},${cos(angle2)*128+128},${sin(angle2)*128+128})`;
+    //c.strokeStyle = "rgb("+[sin(angle2*2),cos(angle2),sin(angle2)].map(v=>v*128+128).join(",")+")";
+    c.strokeStyle = `rgb(${[sin(angle2*2),cos(angle2),sin(angle2)].map(v=>v*128+128).join(",")})`;
 
     var project = (p) => {
-        var rotated4D = mmv([
-            [cos(angle2/4), -sin(angle2/4)],
-            [sin(angle2/4), cos(angle2/4)],
+        var rotated4D = M([
+            [cos(k=angle2/4), -sin(k)],
+            [sin(k), cos(k)],
             [0, 0, cos(angle2), -sin(angle2)],
             [0, 0, sin(angle2), cos(angle2)]
 
-        ], p);
+        ], p.map(e=>[e]));
 
         var w = 1 / (8 - rotated4D[3]);
-        var projectedTo3D = mmv([
-            [w, 0, 0],
-            [0, w, 0],
+        var projectedTo3D = M([
+            [w],
+            [0, w],
             [0, 0, w]
         ], rotated4D);
 
-        var rotated3d = mmv([
+        var rotated3d = M([
             [cos(angle), 0, -sin(angle)],
-            [0, 1, 0],
+            [0, 1],
             [sin(angle), 0, cos(angle)]
         ],  projectedTo3D);
 
         var z = 1 / (6-rotated3d[2]);
         var proj2d = [
-            [z, 0],
+            [z],
             [0, z]
         ];
 
-        return mmv(proj2d, rotated3d).map(v => v * w*30000);
+        return M(proj2d, rotated3d).map(v => v[0] * w*30000);
     }
     var C = (i, j) => {
         c.beginPath();
@@ -88,7 +90,7 @@ with(Math)(L = () => {
 
     for (i = 16; i--;)
         for (j = i; j--;)
-            if (dt(P[i], P[j]) == 4) C(i, j);
+            if (dt(P[i], P[j]) == 1) C(i, j);
            // console.log(i,j,dt(P[i], P[j]));
 
 
