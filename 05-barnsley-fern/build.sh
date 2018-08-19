fbc070fa0be86b2066e47005583a0584495603b2
Florian <f.maul@fme.de>
2018-08-19 08:57:43 +0200

    Fern beautify

--- 05-barnsley-fern/build.sh
+++ 05-barnsley-fern/build.sh
@@ -3,4 +3,4 @@
 uglifyjs index.js -c -m toplevel=true,eval=true > index.min.js
 wc index.min.js
 
-cat shim.1.html index.min.js shim.2.html > shim.html
+#cat shim.1.html index.min.js shim.2.html > shim.html
--- 05-barnsley-fern/index.js
+++ 05-barnsley-fern/index.js
@@ -1,13 +1,24 @@
-h = a.height,
-    w = a.width, x = y = 0,n=100;
+h = a.height;
+w = a.width;
+x = y = 0;
+n = 100;
 
 setInterval(() => {
     for (i = n; i--;) {
-        r = Math.random()*n;
-        if (r < 1) z = 0, y = 0.16 * y;
-        else if (r < 86) z = 0.85 * x + 0.04 * y, y = .85 * y - .04 * x + 1.6;
-        else if (r < 93) z = 0.2 * x - 0.26 * y, y = .23 * x + 0.22 * y + 1.6;
-        else z = 0.28 * y - 0.15 * x, y = 0.26 * x + 0.24 * y + 0.44;
+        r = Math.random() * 100;
+        if (r < 1) {
+            z = 0;
+            y = 0.16 * y;
+        } else if (r < 86) {
+            z = 0.85 * x + 0.04 * y;
+            y = 0.85 * y - 0.04 * x + 1.6;
+        } else if (r < 93) {
+            z = 0.2 * x - 0.26 * y;
+            y = 0.23 * x + 0.22 * y + 1.6;
+        } else {
+            z = 0.28 * y - 0.15 * x;
+            y = 0.26 * x + 0.24 * y + 0.44;
+        }
         x = z;
         c.fillRect(w / 2 + x * n, h - y * n, 1, 1);
     }
